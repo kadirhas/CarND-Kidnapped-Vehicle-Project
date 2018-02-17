09277e459a5947f4a25205604fe7c5d3130190f4
kadir <kadirhas@sabanciuniv.edu>
2018-02-17 17:42:16 +0300

    Update weights function is added. Association function is added

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -63,8 +63,8 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 		for (int i = 0; i<num_particles; i++)
 		{
 			//you also need to add std deviation
-			particles[i].x += velocity*delta_t*cos(yaw0);
-			particles[i].y += velocity*delta_t*sin(yaw0);
+			particles[i].x += velocity*delta_t*cos(particles[i].theta);
+			particles[i].y += velocity*delta_t*sin(particles[i].theta);
 		}
 	}
 	else
@@ -72,15 +72,19 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 		const double yawdt = yaw_rate*delta_t;
 		for (int i = 0; i<num_particles; i++)
 		{
+			double theta_0 = particles[i].theta;
 			//add noise
 			particles[i].x += (velocity/yaw_rate)*(sin(theta_0+yawdt) - sin(theta_0));
 			particles[i].y += (velocity/yaw_rate)*(cos(theta_0) - cos(theta_0+yawdt));
 			particles[i].theta += yawdt;
 		}
 	}
-	particles[i].x += dist_x(gen);
-	particles[i].y += dist_y(gen);
-	particles[i].theta += dist_theta(gen);
+	for (int i = 0; i<num_particles; i++)
+	{
+		particles[i].x += dist_x(gen);
+		particles[i].y += dist_y(gen);
+		particles[i].theta += dist_theta(gen);
+	}
 
 }
 
@@ -89,7 +93,22 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	//   observed measurement to this particular landmark.
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
 	//   implement this method and use it as a helper during the updateWeights phase.
+	for (int obv = 0; obv < observations.size(); obv++)
+	{
+		double min_dist = 100000000000;
+		int prdid = -1;
+		for (int pr = 0; pr<predicted.size(); pr ++)
+		{
+			double distance = dist(observations[obv].x, observations[obv].y, predicted[pr].x, predicted[pr].y);
+			if (distance<min_dist)
+			{
+				min_dist = distance;
+				prdid = predicted[pr].id;
+			}
+		}
 
+		observations[obv].id = prdid;
+	}
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
@@ -104,7 +123,59 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		//   and the following is a good resource for the actual equation to implement (look at equation
 		//   3.33
 		//   http://planning.cs.uiuc.edu/node99.html
-	}
+		//std_landmark = sigma_landmark [2] = {0.3, 0.3}; // Landmark measurement uncertainty [x [m], y [m]]
+		for (int i = 0; i < num_particles; i++)
+		{
+			double px = particles[i].x;
+			double py = particles[i].y;
+			double ptheta = particles[i].theta;
+			//initiate particle weight for calculation
+			particles[i].weight = 1.0;
+			vector<LandmarkObs> obsPredictions;
+			// only consider the landmarks in the sensor range
+			for (int j = 0; j<map_landmarks.landmark_list.size(); j++)
+			{
+				float lmx = map_landmarks.landmark_list[j].x_f;
+				float lmy = map_landmarks.landmark_list[j].y_f;
+				if (dist(px,py,lmx,lmy)<sensor_range)
+				{
+					obsPredictions.push_back(LandmarkObs{map_landmarks.landmark_list[j].id_i,lmx,lmy});
+				}
+			}
+			//transform sensor obstacles to global coordinates
+			vector<LandmarkObs> obsGlobal;
+
+			for (int obs = 0; obs < observations.size(); obs++ )
+			{
+				LandmarkObs temp;
+				temp.x = observations[obs].x * cos(ptheta) - observations[obs].y * sin(ptheta) + px;
+				temp.y = observations[obs].x * sin(ptheta) - observations[obs].y * cos(ptheta) + py;
+				temp.id = observations[obs].id;
+				obsGlobal.push_back(temp);
+			}
+			//associate them for each particle
+			dataAssociation(obsPredictions,obsGlobal);
+			//calculate the weights for each landmark
+			for (int obs = 0; obs < obsGlobal.size(); obs++)
+			{
+				double predictionX;
+				double predictionY;
+				int currentID = obsGlobal[obs].id;
+				//get the matched prediction
+				for (int match = 0; match < obsPredictions.size(); match++)
+				{
+					if (currentID==obsPredictions[match].id)
+					{
+						predictionX = obsPredictions[match].x;
+						predictionY = obsPredictions[match].y;
+						break;
+					}
+				}//for (int match = 0; match < obsPredictions.size(); match++)
+				//weight calculation and production with the earlier ones
+				particles[i].weight *= exp( -(pow(predictionX-obsGlobal[obs].x,2)/(2*pow(std_landmark[0],2))+(pow(predictionY-obsGlobal[obs].y,2)/(2*pow(std_landmark[1],2)) )) )/(2*M_PI*std_landmark[0]*std_landmark[1]);
+			}//for (int obs = 0; obs < obsGlobal.size(); obs++)
+		} //end of particle loop
+	} // end of update weights function
 
 	void ParticleFilter::resample() {
 		// TODO: Resample particles with replacement with probability proportional to their weight.
